9dc021783ab28b2024d54de0937f9c01d237067b
Nithya <nithyaasworld@gmail.com>
2021-02-13 20:23:35 +0530

    Updated Logo and removed unnecessary comments

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,7 +4,7 @@ import logoStyles from './Logo.module.css';
 const Logo = () => {
     return (
         <div className={logoStyles.flexContainer}>
-            <img src={process.env.PUBLIC_URL + "/logo.jpg"} alt="Sholai School Logo"/>
+            <img src={process.env.PUBLIC_URL + "/logo2.png"} alt="Sholai School Logo"/>
             <div className={logoStyles.logoText}>
                 <h1><span className={logoStyles.headerTextBlue}>Sholai</span> <span className={logoStyles.headerTextGreen}>CLOAAT</span></h1>
                 <h2>A very different education</h2>
